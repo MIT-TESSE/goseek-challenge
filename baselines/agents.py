69280bb0343577bbd92966f088aae807b2d85e6b
Zachary Ravichandran <zachary.ravichandran@ll.mit.edu>
2020-02-10 12:56:02 -0500

    inital eval code commit

---
+++ baselines/agents.py
@@ -0,0 +1,45 @@
+from typing import Any, Dict
+
+import numpy as np
+from stable_baselines import PPO2
+
+from tesse_gym.eval.agent import Agent
+
+
+class StableBaselinesPPO(Agent):
+    """ stable-baselines PPO agent for GOSEEK submission. """
+
+    def __init__(self, config: Dict[str, Any]) -> None:
+        """ Initialize agent.
+
+        Args:
+            config (Dict[str, Any]): Agent configuration.
+        """
+        self.model = PPO2.load(config["weights"])
+        self.state = None
+
+        # Number of environments used to train model
+        # to which stable-baselines input tensor size is fixed
+        self.n_train_envs = self.model.initial_state.shape[0]
+
+    def act(self, observation: np.ndarray) -> int:
+        """ Act upon an observation.
+
+        args:
+            observation (np.ndarray): observation.
+
+        returns:
+            int: an action in [0, 4) defined as follows
+                - 0: forward 0.5m
+                - 1: right 8 degrees
+                - 2: left 8 degrees
+                - 3: declare target
+        """
+        observation = np.repeat(observation[np.newaxis], self.n_train_envs, 0)
+        actions, state = self.model.predict(observation, state=self.state, deterministic=False)
+        self.state = state  # update model state
+        return actions[0]
+
+    def reset(self) -> None:
+        """ Reset model state. """
+        self.state = None
---
+++ eval.py
@@ -0,0 +1,47 @@
+import argparse
+import pprint
+from typing import Any, Dict
+
+import yaml
+
+from baselines.agents import *  # specific agent is given in agent-config
+from tesse_gym.eval.agent import Agent
+from tesse_gym.eval.treasure_hunt_benchmark import TreasureHuntBenchmark
+from tesse_gym.eval.utils import get_agent_cls
+
+
+def main(env_args: Dict[str, Any], agent_args: Dict[str, Any]):
+    """ Run GOSEEK evaluation over the specified environment and
+    agent configurations.
+
+    Args:
+        env_args (Dict[str, Any]): Environment configurations.
+        agent_args (Dict[str, Any]): Agent configurations.
+    """
+    benchmark = TreasureHuntBenchmark(**env_args)
+    agent = get_agent_cls(agent_args["name"], Agent)(agent_args)
+    return benchmark.evaluate(agent)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env-config", type=str)
+    parser.add_argument("--agent-config", type=str)
+    args = parser.parse_args()
+
+    with open(args.env_config) as f:
+        env_args = yaml.load(f, Loader=yaml.FullLoader)
+
+    with open(args.agent_config) as f:
+        agent_args = yaml.load(f, Loader=yaml.FullLoader)
+
+    results = main(env_args, agent_args)
+
+    print("------ Environment Configuration -----")
+    pprint.pprint(env_args, depth=2)
+
+    print("\n----- Agent Configuration -----")
+    pprint.pprint(agent_args, depth=2)
+
+    print("\n----- Per Episode Score -----")
+    pprint.pprint(results, depth=2)
