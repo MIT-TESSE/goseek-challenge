fab3549fc0cb084abae3309209a103d19664f263
Dan Griffith <dan.griffith@ll.mit.edu>
2020-02-19 17:25:37 -0500

    Update temporary-clones.sh

--- docker/goseek-base/temporary-clones.sh
+++ docker/goseek-base/temporary-clones.sh
@@ -2,5 +2,5 @@
 # when repositories are on github.com this file will be removed
 # TODO(MMAZ) when doing so, also edit goseek-challenge/docker/.gitignore
 git clone git@github.mit.edu:TESS/goseek-challenge.git
-git clone git@github.mit.edu:TESS/tesse-gym.git -b 0.1.1-SNAPSHOT
+git clone git@github.mit.edu:TESS/tesse-gym.git
 git clone git@github.mit.edu:TESS/tesse-interface.git
